caa3ed09a190587f36b7958d6a74984ab2b927d6
Shweta Iyer <si9808@rit.edu>
2022-11-19 15:08:40 -0500

    added changes for navbar

--- src/home/navbar.js
+++ src/home/navbar.js
@@ -1,50 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Sidenav, Nav } from 'rsuite';
 import './navbar.css';
 
 export const NavigationBar = ({adminuser}) => {
   // TODO: Call api for teams
-  
-const teams = [{name: "team 1"}, {name: "team 2"}, {name: "team3"}]
 
-  return (
-    <div style={{ width: 240 }}>
-    <Sidenav defaultOpenKeys={['3', '4']}>
-      <Sidenav.Body>
-        <Nav activeKey="1" style={{height: 900}}>
-          <Nav.Menu eventKey="1" title="Settings">
-            <Nav.Item eventKey="4-1">Applications</Nav.Item>
-            <Nav.Item eventKey="4-2">Channels</Nav.Item>
-            <Nav.Item eventKey="4-3">Versions</Nav.Item>
-            <Nav.Menu eventKey="4-5" title="Custom Action">
-              <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-              <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-            </Nav.Menu>
-          </Nav.Menu>
-          <Nav.Item eventKey="1-1">
-            Tasks
-          </Nav.Item>
-          <Nav.Item eventKey="1-2">
-            Teamboard
-          </Nav.Item>
-          <Nav.Item eventKey="1-3">
-            Meetings
-          </Nav.Item>
-          {/* <Nav.Menu eventKey="4" title="Settings">
-            <Nav.Item eventKey="4-1">Applications</Nav.Item>
-            <Nav.Item eventKey="4-2">Channels</Nav.Item>
-            <Nav.Item eventKey="4-3">Versions</Nav.Item>
-            <Nav.Menu eventKey="4-5" title="Custom Action">
-              <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-              <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-            </Nav.Menu>
-          </Nav.Menu> */}
-        </Nav>
-      </Sidenav.Body>
-    </Sidenav>
-  </div>
-  )
-}
+  const headerStyles = {
+    padding: 20,
+    fontSize: 16,
+    background: '#34c3ff',
+    color: ' #fff'
+  };
+
+  const getTeamNav = (id, name) => {
+    console.log(id)
+    console.log(name)
+    return (
+      <Nav.Menu eventKey={id} title={name}>
+        <Nav.Item eventKey={`${id}-1`}>Tasks</Nav.Item>
+        <Nav.Item eventKey={`${id}-2`}>Teamboard</Nav.Item>
+        <Nav.Item eventKey={`${id}-3`}>Meetings</Nav.Item>
+      </Nav.Menu>
+    )
+  }
+  
+  const teams = [{name: "team 1"}, {name: "team 2"}, {name: "team3"}]
+
+    return (
+      <div style={{ width: 240 }}>
+      <Sidenav defaultOpenKeys={['3', '4']}>
+        <Sidenav.Header>
+          <div style={headerStyles}>Teamster</div>
+        </Sidenav.Header>
+        <Sidenav.Body>
+          <Nav activeKey="1" style={{height: 900}}>
+            {teams.map((team,index) => { 
+                return getTeamNav(index, team.name)
+              })
+            }
+            
+            <Nav.Item eventKey="100">
+              Planner
+            </Nav.Item>
+            <Nav.Item eventKey="200">
+              Admin Configutaion
+            </Nav.Item>
+          </Nav>
+        </Sidenav.Body>
+      </Sidenav>
+    </div>
+    )
+  }
 
 // // import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
 // // import styled from 'styled-components';
