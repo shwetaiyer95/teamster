474b908b63db9d8be2d5b2cdc3354c1e36c4acc4
Ritesh091 <riteshjain462@gmail.com>
2022-11-19 16:55:24 -0500

    Database Creation and Endpoints

---
+++ database_create.sql
@@ -0,0 +1,91 @@
+Use teamster;
+CREATE TABLE `user_table` (
+  `userid` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `userType` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`userid`)
+  );
+
+CREATE TABLE `team` (
+  `teamID` varchar(45) NOT NULL,
+  `teamName` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`teamID`)
+  );
+
+CREATE TABLE `user_detail` (
+  `userid` varchar(45) NOT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `user_table` (`userid`)
+  );
+
+
+CREATE TABLE `habits` (
+  `habitId` varchar(45) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `recurring` enum('hourly','daily','weekly','monthly') DEFAULT NULL,
+  `habitName` varchar(45) NOT NULL,
+  PRIMARY KEY (`habitId`)
+  );
+
+CREATE TABLE `meeting_details` (
+  `meetingID` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  PRIMARY KEY (`meetingID`)
+  );
+
+CREATE TABLE `task` (
+  `taskId` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `duration` datetime NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `assignedId` varchar(45) NOT NULL,
+  PRIMARY KEY (`taskId`),
+  KEY `userid_idx` (`assignedId`),
+  CONSTRAINT `assignedId` FOREIGN KEY (`assignedId`) REFERENCES `user_table` (`userid`)
+  );
+
+
+CREATE TABLE `member` (
+  `teamID` varchar(45) NOT NULL,
+  `memberId` varchar(45) NOT NULL,
+  KEY `teamID_idx` (`teamID`),
+  KEY `userid_idx` (`memberId`),
+  CONSTRAINT `memberId` FOREIGN KEY (`memberId`) REFERENCES `user_table` (`userid`),
+  CONSTRAINT `teamID` FOREIGN KEY (`teamID`) REFERENCES `team` (`teamID`)
+  );
+
+
+CREATE TABLE `task_team` (
+  `taskid` varchar(45) NOT NULL,
+  `Idteam` varchar(45) NOT NULL,
+  KEY `taskid_idx` (`taskid`),
+  KEY `teamid_idx` (`Idteam`),
+  CONSTRAINT `Idteam` FOREIGN KEY (`Idteam`) REFERENCES `team` (`teamID`),
+  CONSTRAINT `taskid` FOREIGN KEY (`taskid`) REFERENCES `task` (`taskId`)
+  );
+
+
+CREATE TABLE `user_habit` (
+  `IDuser` varchar(45) NOT NULL,
+  `habitId` varchar(45) NOT NULL,
+  KEY `userId_idx` (`IDuser`),
+  KEY `habitId_idx` (`habitId`),
+  CONSTRAINT `habitId` FOREIGN KEY (`habitId`) REFERENCES `habits` (`habitId`),
+  CONSTRAINT `IDuser` FOREIGN KEY (`IDuser`) REFERENCES `user_table` (`userid`)
+  );
+
+CREATE TABLE `meeting_user` (
+  `meetingID` varchar(45) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  KEY `meetingID_idx` (`meetingID`),
+  KEY `IDuser_idx` (`user_id`),
+  CONSTRAINT `meetingID` FOREIGN KEY (`meetingID`) REFERENCES `meeting_details` (`meetingID`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`userid`)
+  );
---
+++ db_connection.py
@@ -0,0 +1,145 @@
+import pymysql.cursors
+from flask import Flask, request, jsonify
+from flask import make_response
+
+HOST = "127.0.0.1"
+PORT = 3306
+USER = "root"
+PASSWORD = "password@123"
+DBNAME = "sys"
+
+app = Flask(__name__)
+
+connection = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, database=DBNAME)
+
+def connect():
+    conn = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, database=DBNAME)
+    return conn
+
+def create_user(name, email, passwd, usertype):
+    try:
+        conn = connect()
+        with conn.cursor() as cur:
+            sql = "INSERT INTO user_table(userid, email, password, name, userType) values(UUID(), '%s','%s','%s', '%s');" % (email, passwd, name, usertype)
+            cur.execute(sql)
+            conn.commit()
+            return True
+    except Exception:
+
+        return False
+
+def create_team(name):
+    try:
+        conn = connect()
+        with conn.cursor() as cur:
+            sql = "INSERT INTO team(teamID, teamName) values(UUID(),'%s');" % (name)
+            cur.execute(sql)
+            conn.commit()
+            return True
+    except Exception:
+        return False
+
+def create_user_details(uid, pom_start, pom_end):
+    try:
+        conn = connect()
+        with conn.cursor() as cur:
+            sql = "INSERT INTO team(userid, start_time, end_time) values('%s', '%s', '%s');" % (uid, pom_start, pom_end)
+            cur.execute(sql)
+            conn.commit()
+            return True
+    except Exception:
+        return False
+
+def create_habits(name, start_time, end_time, recur_type):
+    try:
+        conn = connect()
+        with conn.cursor() as cur:
+            sql = "INSERT INTO habits(habitid, name, start_time, end_time, recurring) values(UUID(), '%s', '%s', '%s', '%s');" % (name, start_time, end_time, recur_type)
+            cur.execute(sql)
+            conn.commit()
+            return True
+    except Exception:
+        return False
+
+def create_meeting(name, start_time, end_time, recur_type):
+    try:
+        conn = connect()
+        with conn.curosr() as cur:
+            sql = "INSERT INTO meeting_details(meetingID, name, start_time, end_time, recurring) values(UUID(), '%s', '%s', '%s', '%s');" % (name, start_time, end_time, recur_type)
+            cur.execute(sql)
+            conn.commit()
+            return True
+    except Exception:
+        return False
+
+def create_task(name, assigned, duration, description):
+    try:
+        conn = connect()
+        with conn.curosr() as cur:
+            sql = "INSERT INTO task(taskID, name, duration, description, assigned) values(UUID(), '%s', '%s', '%s', '%s');" % (name, duration, description, assigned)
+            cur.execute(sql)
+            conn.commit()
+            return True
+    except Exception:
+        return False
+
+@app.route('/create_team', methods=['POST'])
+def team_creator():
+    name = request.json.get('name')
+    if name is not None:
+        if create_team(name):
+            return make_response(jsonify({'message': 'Team created'}), 200)
+
+@app.route('/create_user_details', methods=['POST'])
+def user_detail_creator():
+    uid, pom_start, pom_end
+    uid = request.json.get('uid')
+    pom_start = request.json.get('pom_start')
+    pom_end = request.json.get('pom_end')
+    if uid is not None and pom_start is not None and pom_end is not None:
+        if create_user_details(uid, pom_start, pom_end):
+            return make_response(jsonify({'message': 'User details created'}, 200))
+    
+    return make_response(jsonify({}), 400)
+
+@app.route('/create_habit', method=['POST'])
+def habit_creator():
+    name = request.json.get('name')
+    start_time = request.json.get('start_time')
+    end_time = request.json.get('end_time')
+    recur_type = request.json.get('recur_type')
+    if name is not None and start_time is not None and end_time is not None and recur_type is not None:
+        if create_habits(name, start_time, end_time, recur_type):
+            return make_response(jsonify({'message': 'Habit created'}, 200))
+    
+    return make_response(jsonify({}), 400)
+
+
+# create_task(name, assigned, duration, description)
+@app.route('/create_task', method=['POST'])
+def task_creator():
+    name = request.json.get('name')
+    assigned = request.json.get('assigned')
+    duration = request.json.get('duration')
+    description = request.json.get('description')
+    if name is not None and assigned is not None and duration is not None and description is not None:
+        if create_habits(name, assigned, duration, description):
+            return make_response(jsonify({'message': 'Habit created'}, 200))
+    
+    return make_response(jsonify({}), 400)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.json.get('email')
+    name = request.json.get('name')
+    passwd = request.json.get('password')
+    usertype = request.json.get('usertype')
+    if email is not None and name is not None and passwd is not None and usertype is not None:
+        if create_user(name, email, passwd, usertype):
+            return make_response(jsonify({'message': 'User created'}), 200)
+    
+    return make_response(jsonify({}), 400)
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
