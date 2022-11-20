ae8fbb9786becd51084d3d9e4b26fc9a24a5366e
Shweta Iyer <si9808@rit.edu>
2022-11-20 11:10:17 -0500

    added timer in planner

--- src/home/plannerhome.js
+++ src/home/plannerhome.js
@@ -1,14 +1,28 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CardComponent from "./cardComponent"
 import './cardComponent.css'
 import { Modal, Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { useParams } from 'react-router-dom';
+import Axios from "axios"
+// import PomTimer from "./timer";
+import { useTimer } from 'react-timer-hook';
+import './plannerhome.css'
+
 export const PlannerHome = () => {
     const { userId } = useParams();
     const [isPomodoroModalOpen, setIsPomodoroModalOpen] = useState(false)
-    const [focusTime, setFocusTime] = useState(0)
-    const [breakTime, setBreakTime] = useState(0)
+    const [focusTime, setFocusTime] = useState(2)
+    const [breakTime, setBreakTime] = useState(1)
+    const[currentTime, setCurrentTime] = useState("Focus Time")
+    const [time, setTime] = useState(new Date())
+    const cardText = `Focus Time and Relax Time are  ${focusTime} and ${breakTime} minutes respectively`
+    
+    useEffect(() => {
+        const newTime = time
+        newTime.setSeconds(time.getSeconds() + 10)
+        setTime(newTime); // 10 minutes timer
+    },[])
 
     const handleUpdatePomodoroModalOpen = () => {
         setIsPomodoroModalOpen(true)
@@ -19,8 +33,61 @@ export const PlannerHome = () => {
     }
 
     const handlePomodoroSubmit = (e) => {
-        
     }
+
+    useEffect(() => {
+        const HOST = "http://127.0.0.1:5000";
+        Axios.get(`${HOST}/get_focus_break_time/${userId}`)
+        .then((response) => {
+            setFocusTime(response.data)
+        })
+        .catch((error) => {
+            const data = error.response.data
+            if (error.response.status === 400) {
+                console.log("something went wrong")
+            }
+        })
+    }, [])
+
+    function MyTimer({ expiryTimestamp }) {
+        const {
+          seconds,
+          minutes,
+          hours,
+          days,
+          isRunning,
+        //   start,
+        //   pause,
+        //   resume,
+        //   restart,
+        } = useTimer({ expiryTimestamp, onExpire: () => handleExpire() });
+      
+        const handleExpire = () => {
+            if(currentTime === 'Focus Time') {
+                setCurrentTime('Break Time')
+                const newTime = time
+                newTime.setSeconds(time.getSeconds() + breakTime*60)
+                setTime(newTime); 
+            } else {
+                setCurrentTime('Focus Time')
+                const newTime = time
+                newTime.setSeconds(time.getSeconds() + focusTime*60)
+                setTime(newTime); 
+            }
+        }
+
+        return (
+          <div className="titlebar">
+            <p className="titlebarname">
+               Planner
+            </p>
+            <p className="titlebarname">
+              <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+              <span>{`  ${currentTime}`}</span>
+            </p>
+          </div>
+        );
+      }
 
     function createPomodoroForm(){
         return(
@@ -42,8 +109,9 @@ export const PlannerHome = () => {
     }
 
     return (
-        <div>
-            <CardComponent handleModalOpen = {handleUpdatePomodoroModalOpen} title="Pomodoro Timer" subtitle="Your Pomodoro Time" text="Focus Time: Relax Time:" link="Change time"></CardComponent>
+        <div className="meetingWrapper">
+            <MyTimer expiryTimestamp={time} />
+            <CardComponent handleModalOpen = {handleUpdatePomodoroModalOpen} title="Pomodoro Timer" subtitle="Your Pomodoro Time" text={cardText} link="Change time"></CardComponent>
             <Modal show={isPomodoroModalOpen} onHide={handleUpdatePomodoroModalClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Update Pomodoro time</Modal.Title>
